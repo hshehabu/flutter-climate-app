0cb3b02c08f6c1951fb86bb66931d0a914201822
hshehabu <hamzashehabu457@gmail.com>
2023-07-11 15:03:08 +0300

    modularize location weather data to weather.dart

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,10 +1,7 @@
 import 'package:clima/screens/location_screen.dart';
+import 'package:clima/services/weather.dart';
 import 'package:flutter/material.dart';
-import 'package:clima/services/location.dart';
-import '../services/networking.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
-
-const apiKey = 'e99dc34eaffb43e91795f4bd97291ceb';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -19,12 +16,8 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   void getLocationData() async {
-    Location location = Location();
-    await location.getCurrentLocation();
-    NetworkHelper networkHelper = NetworkHelper(
-        url:
-            'https://api.openweathermap.org/data/2.5/weather?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
-    var weatherData = await networkHelper.getData();
+
+    var weatherData = await WeatherModel().getLocationWeather();
 
     Navigator.push(context, MaterialPageRoute(builder: (context) {
       return LocationScreen(
--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,21 @@
+import 'package:clima/services/location.dart';
+import '../services/networking.dart';
+
+const apiKey = 'e99dc34eaffb43e91795f4bd97291ceb';
+const openWeatherMapURL = 'https://api.openweathermap.org/data/2.5/weather';
+
 class WeatherModel {
+
+  Future<dynamic> getLocationWeather() async{
+  Location location = Location();
+  await location.getCurrentLocation();
+  NetworkHelper networkHelper = NetworkHelper(
+      url:
+      '$openWeatherMapURL?lat=${location.latitude}&lon=${location.longitude}&appid=$apiKey&units=metric');
+  var weatherData = await networkHelper.getData();
+  return weatherData;
+}
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
